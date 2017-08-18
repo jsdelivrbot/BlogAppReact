f157a5b70bd9640836a238bcc6f4f0d1de800664
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2017-08-18 18:06:01 -0400

    initialize app

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+import PostsIndex from './components/posts_index';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
