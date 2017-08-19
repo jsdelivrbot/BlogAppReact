15de50fc63469e7a3ed7f4d5e15a9be57a96af5e
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2017-08-19 16:26:34 -0400

    adding no post to api and view  done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const APY_KEY = '?key=njanvjneruavnrjvnvebbibirueuvaeu';
@@ -10,6 +11,16 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${APY_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
