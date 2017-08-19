0abd4d27bc64c1d7669f5ed6cf5242629a1732fc
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2017-08-19 00:16:45 -0400

    added errors

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -24,8 +24,8 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -38,6 +38,25 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    errors.title = "Enter a title!"
+  }
+
+  if(!values.categories) {
+    errors.title = "Enter some categories!"
+  }
+
+  if(!values.content) {
+    errors.title = "Enter a post content!"
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
