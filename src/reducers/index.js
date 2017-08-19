965a21f91426cb050946a6745ee36c55242c0d4f
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2017-08-18 21:58:20 -0400

    added inputs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
-  form: fromReducer
+  form: formReducer
 });
 
 export default rootReducer;
